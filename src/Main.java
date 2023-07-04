b1fd87bac628b63bb1ed0184f7a4ea06f7f8b636
Ívano Fontes <ivanofnts@gmail.com>
2023-07-04 02:23:53 -0300

    levemente melhorada

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,50 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int x = Integer.parseInt(args[1]);
-        int y = Integer.parseInt(args[2]);
+        Scanner scanner = new Scanner(System.in);
 
-        if(args[0].equals("soma")) {
+        System.out.print("Qual a operação? ");
+        String operacao = scanner.nextLine();
+
+        System.out.print("Digite o primeiro número: ");
+        int x = scanner.nextInt();
+
+        System.out.print("Digite o segundo número: ");
+        int y = scanner.nextInt();
+
+        if (operacao.equals("soma")) {
             sum(x, y);
-        } else if(args[0].equals("subtracao")) {
+        } else if (operacao.equals("subtração")) {
             minus(x, y);
-        } else if(args[0].equals("multiplicacao")) {
+        } else if (operacao.equals("multiplicação")) {
             multiplication(x, y);
-        } else if(args[0].equals("divisao")) {
+        } else if (operacao.equals("divisão")) {
             division(x, y);
-        } else if(args[0].equals("modulo")) {
+        } else if (operacao.equals("módulo")) {
             module(x, y);
         } else {
-            System.out.println("Nenhuma instrução definida!");
+            System.out.println("Operação inválida!");
         }
     }
 
     static void sum(int x, int y) {
-        System.out.println(x + y);
+        System.out.println("O resultado da soma é " + (x + y));
     }
 
     static void minus(int x, int y) {
-        System.out.println(x - y);
+        System.out.println("O resultado da subtração é " + (x - y));
     }
 
     static void multiplication(int x, int y) {
-        System.out.println(x * y);
+        System.out.println("O resultado da multiplicação é " + (x * y));
     }
 
     static void division(int x, int y) {
-        System.out.println(x / y);
+        System.out.println("O resultado da divisão é " + (x / y));
     }
 
     static void module(int x, int y) {
-        System.out.println(x % y);
+        System.out.println("O módulo da divisão é " + (x % y));
     }
 }
